ddcc17482c5c2fe2f2505114fc4c14612fef2c1b
Keiho Sakapon <git@saka-pon.net>
2018-02-18 03:44:42 +0900

    Add test

---
+++ ConversionSample/UnitTest/UriTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UriTest
+    {
+        static readonly short[] PercentEncodeSet = new short[] { 32, 34, 35, 60, 62, 96, 123, 125, 126 };
+
+        [TestMethod]
+        public void EscapeUriString_1()
+        {
+            var ascii = string.Join("", Enumerable.Range(32, 127 - 32).Select(i => (char)(short)i));
+            var ascii2 = Uri.EscapeUriString(ascii);
+
+            Assert.AreEqual("%E3%81%82", Uri.EscapeUriString("あ"));
+        }
+    }
+}
