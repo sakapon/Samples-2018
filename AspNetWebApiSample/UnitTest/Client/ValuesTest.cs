e5229c13ba14809947605f6dba143fd117356f6b
Keiho Sakapon <git@saka-pon.net>
2018-03-15 17:21:05 +0900

    Add test

---
+++ AspNetWebApiSample/UnitTest/Client/ValuesTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest.Client
+{
+    [TestClass]
+    public class ValuesTest
+    {
+        [TestMethod]
+        async public Task Get()
+        {
+            using (var http = new HttpClient { BaseAddress = new Uri("http://localhost:1961/") })
+            {
+                var response = await http.GetAsync("api/values");
+                var result = await response.Content.ReadAsAsync<string[]>();
+
+                Assert.IsNotNull(result);
+                Assert.AreEqual(2, result.Length);
+                Assert.AreEqual("value1", result[0]);
+                Assert.AreEqual("value2", result[1]);
+            }
+        }
+    }
+}
