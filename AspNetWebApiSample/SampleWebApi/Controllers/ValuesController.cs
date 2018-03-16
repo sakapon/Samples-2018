99718fe3e45b86289393ee64d3d8fa11bcc3e0e2
Keiho Sakapon <git@saka-pon.net>
2018-03-16 16:18:30 +0900

    Add comments

--- AspNetWebApiSample/SampleWebApi/Controllers/ValuesController.cs
+++ AspNetWebApiSample/SampleWebApi/Controllers/ValuesController.cs
@@ -9,29 +9,51 @@ namespace SampleWebApi.Controllers
 {
     public class ValuesController : ApiController
     {
-        // GET api/values
+        /// <summary>
+        /// Gets all values.
+        /// </summary>
+        /// <returns>All values.</returns>
+        /// <remarks>GET api/values</remarks>
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
-        // GET api/values/5
+        /// <summary>
+        /// Gets the value for the specified ID.
+        /// </summary>
+        /// <param name="id">The ID.</param>
+        /// <returns>The value.</returns>
+        /// <remarks>GET api/values/5</remarks>
         public string Get(int id)
         {
             return "value";
         }
 
-        // POST api/values
+        /// <summary>
+        /// Posts the new value.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <remarks>POST api/values</remarks>
         public void Post([FromBody]string value)
         {
         }
 
-        // PUT api/values/5
+        /// <summary>
+        ///  Updates the value for the specified ID.
+        /// </summary>
+        /// <param name="id">The ID.</param>
+        /// <param name="value">The value.</param>
+        /// <remarks>PUT api/values/5</remarks>
         public void Put(int id, [FromBody]string value)
         {
         }
 
-        // DELETE api/values/5
+        /// <summary>
+        ///  Deletes the value for the specified ID.
+        /// </summary>
+        /// <param name="id">The ID.</param>
+        /// <remarks>DELETE api/values/5</remarks>
         public void Delete(int id)
         {
         }
