7d88c5c77cd84a7c6e51dcd0e6584cb9adf1e15c
Keiho Sakapon <git@saka-pon.net>
2018-03-25 21:08:43 +0900

    Set sample objects

--- AspNetWebApiSample/SampleWebApi/Areas/HelpPage/App_Start/HelpPageConfig.cs
+++ AspNetWebApiSample/SampleWebApi/Areas/HelpPage/App_Start/HelpPageConfig.cs
@@ -36,14 +36,15 @@ namespace SampleWebApi.Areas.HelpPage
             // Uncomment the following to use the documentation from XML documentation file.
             config.SetDocumentationProvider(new XmlDocumentationProvider(HttpContext.Current.Server.MapPath("~/bin/SampleWebApi.xml")));
 
-            //// Uncomment the following to use "sample string" as the sample for all actions that have string as the body parameter or return type.
-            //// Also, the string arrays will be used for IEnumerable<string>. The sample objects will be serialized into different media type 
-            //// formats by the available formatters.
-            //config.SetSampleObjects(new Dictionary<Type, object>
-            //{
-            //    {typeof(string), "sample string"},
-            //    {typeof(IEnumerable<string>), new string[]{"sample 1", "sample 2"}}
-            //});
+            // Uncomment the following to use "sample string" as the sample for all actions that have string as the body parameter or return type.
+            // Also, the string arrays will be used for IEnumerable<string>. The sample objects will be serialized into different media type 
+            // formats by the available formatters.
+            config.SetSampleObjects(new Dictionary<Type, object>
+            {
+                {typeof(string), "sample string"},
+                {typeof(IEnumerable<string>), new string[]{"sample 1", "sample 2"}},
+                {typeof(double[]), new []{0.123, 0.456}},
+            });
 
             // Extend the following to provide factories for types not handled automatically (those lacking parameterless
             // constructors) or for which you prefer to use non-default property values. Line below provides a fallback
