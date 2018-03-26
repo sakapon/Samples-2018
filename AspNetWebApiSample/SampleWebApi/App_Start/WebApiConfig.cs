e38f9368ae73ced291144f4d3b3055f0d6dd5b6c
Keiho Sakapon <git@saka-pon.net>
2018-03-26 18:54:37 +0900

    Returns JSON on Google Chrome

--- AspNetWebApiSample/SampleWebApi/App_Start/WebApiConfig.cs
+++ AspNetWebApiSample/SampleWebApi/App_Start/WebApiConfig.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Formatting;
 using System.Web.Http;
 using System.Web.Http.Cors;
 
@@ -12,6 +13,11 @@ namespace SampleWebApi
         {
             // Web API の設定およびサービス
             config.EnableCors(new EnableCorsAttribute("*", "*", "*"));
+
+            // Returns JSON on Google Chrome.
+            config.Formatters.JsonFormatter.MediaTypeMappings.Add(new RequestHeaderMapping("Accept", "text/html", StringComparison.InvariantCultureIgnoreCase, true, "application/json"));
+            //config.Formatters.XmlFormatter.SupportedMediaTypes.Clear();
+            //config.Formatters.Remove(config.Formatters.XmlFormatter);
 
             // Web API ルート
             config.MapHttpAttributeRoutes();
