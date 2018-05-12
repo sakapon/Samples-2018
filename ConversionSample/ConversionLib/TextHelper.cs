8a4714a6af10c74977e417f64d2265bb72968d9e
Keiho Sakapon <git@saka-pon.net>
2018-05-12 12:37:15 +0900

    Add classes

---
+++ ConversionSample/ConversionLib/TextHelper.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace ConversionLib
+{
+    public static class TextHelper
+    {
+        public static byte[] Encode(this string text, Encoding encoding)
+        {
+            if (text == null) throw new ArgumentNullException("text");
+            if (encoding == null) throw new ArgumentNullException("encoding");
+
+            return encoding.GetBytes(text);
+        }
+
+        public static string Decode(this byte[] binary, Encoding encoding)
+        {
+            if (binary == null) throw new ArgumentNullException("binary");
+            if (encoding == null) throw new ArgumentNullException("encoding");
+
+            return encoding.GetString(binary);
+        }
+
+        public static byte[] FromBase64String(this string text)
+        {
+            if (text == null) throw new ArgumentNullException("text");
+
+            return Convert.FromBase64String(text);
+        }
+
+        public static string ToBase64String(this byte[] binary)
+        {
+            if (binary == null) throw new ArgumentNullException("binary");
+
+            return Convert.ToBase64String(binary);
+        }
+
+        public static byte[] FromHexString(this string hexString)
+        {
+            if (hexString == null) throw new ArgumentNullException("hexString");
+            if (hexString.Length % 2 != 0) throw new FormatException("入力文字列の長さが偶数ではありません。");
+
+            return Enumerable.Range(0, hexString.Length / 2)
+                .Select(i => hexString.Substring(2 * i, 2))
+                .Select(s => byte.Parse(s, NumberStyles.HexNumber))
+                .ToArray();
+        }
+
+        public static string ToHexString(this byte[] binary, bool lowercase = false)
+        {
+            if (binary == null) throw new ArgumentNullException("binary");
+
+            return new string(binary.SelectMany(b => b.ToString(lowercase ? "x2" : "X2")).ToArray());
+        }
+    }
+}
---
+++ ConversionSample/ConversionLib/XamlHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+using System.Xaml;
+
+namespace ConversionLib
+{
+    public static class XamlHelper
+    {
+        public static byte[] Serialize(this object obj)
+        {
+            if (obj == null) throw new ArgumentNullException("obj");
+
+            return XamlServices.Save(obj).Encode(Encoding.UTF8);
+        }
+
+        public static T Deserialize<T>(this byte[] binary)
+        {
+            if (binary == null) throw new ArgumentNullException("binary");
+
+            return (T)XamlServices.Parse(binary.Decode(Encoding.UTF8));
+        }
+    }
+}
