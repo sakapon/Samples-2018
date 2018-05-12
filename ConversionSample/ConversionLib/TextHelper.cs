7b50a09aba790db3c83527e4b12c969460620644
Keiho Sakapon <git@saka-pon.net>
2018-05-12 13:01:54 +0900

    Refactor

--- ConversionSample/ConversionLib/TextHelper.cs
+++ ConversionSample/ConversionLib/TextHelper.cs
@@ -10,37 +10,37 @@ namespace ConversionLib
     {
         public static byte[] Encode(this string text, Encoding encoding)
         {
-            if (text == null) throw new ArgumentNullException("text");
-            if (encoding == null) throw new ArgumentNullException("encoding");
+            if (text == null) throw new ArgumentNullException(nameof(text));
+            if (encoding == null) throw new ArgumentNullException(nameof(encoding));
 
             return encoding.GetBytes(text);
         }
 
         public static string Decode(this byte[] binary, Encoding encoding)
         {
-            if (binary == null) throw new ArgumentNullException("binary");
-            if (encoding == null) throw new ArgumentNullException("encoding");
+            if (binary == null) throw new ArgumentNullException(nameof(binary));
+            if (encoding == null) throw new ArgumentNullException(nameof(encoding));
 
             return encoding.GetString(binary);
         }
 
         public static byte[] FromBase64String(this string text)
         {
-            if (text == null) throw new ArgumentNullException("text");
+            if (text == null) throw new ArgumentNullException(nameof(text));
 
             return Convert.FromBase64String(text);
         }
 
         public static string ToBase64String(this byte[] binary)
         {
-            if (binary == null) throw new ArgumentNullException("binary");
+            if (binary == null) throw new ArgumentNullException(nameof(binary));
 
             return Convert.ToBase64String(binary);
         }
 
         public static byte[] FromHexString(this string hexString)
         {
-            if (hexString == null) throw new ArgumentNullException("hexString");
+            if (hexString == null) throw new ArgumentNullException(nameof(hexString));
             if (hexString.Length % 2 != 0) throw new FormatException("入力文字列の長さが偶数ではありません。");
 
             return Enumerable.Range(0, hexString.Length / 2)
@@ -51,7 +51,7 @@ namespace ConversionLib
 
         public static string ToHexString(this byte[] binary, bool lowercase = false)
         {
-            if (binary == null) throw new ArgumentNullException("binary");
+            if (binary == null) throw new ArgumentNullException(nameof(binary));
 
             return new string(binary.SelectMany(b => b.ToString(lowercase ? "x2" : "X2")).ToArray());
         }
--- ConversionSample/ConversionLib/XamlHelper.cs
+++ ConversionSample/ConversionLib/XamlHelper.cs
@@ -6,16 +6,16 @@ namespace ConversionLib
 {
     public static class XamlHelper
     {
-        public static byte[] Serialize(this object obj)
+        public static byte[] SerializeByXaml(this object obj)
         {
-            if (obj == null) throw new ArgumentNullException("obj");
+            if (obj == null) throw new ArgumentNullException(nameof(obj));
 
             return XamlServices.Save(obj).Encode(Encoding.UTF8);
         }
 
-        public static T Deserialize<T>(this byte[] binary)
+        public static T DeserializeByXaml<T>(this byte[] binary)
         {
-            if (binary == null) throw new ArgumentNullException("binary");
+            if (binary == null) throw new ArgumentNullException(nameof(binary));
 
             return (T)XamlServices.Parse(binary.Decode(Encoding.UTF8));
         }
