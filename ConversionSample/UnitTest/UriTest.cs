49908f339180f837cf1f82f471773c50eb7b60f1
Keiho Sakapon <git@saka-pon.net>
2018-05-12 17:41:05 +0900

    Update test

--- ConversionSample/UnitTest/UriTest.cs
+++ ConversionSample/UnitTest/UriTest.cs
@@ -2,20 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NetWebUtility = System.Net.WebUtility;
 
 namespace UnitTest
 {
     [TestClass]
     public class UriTest
     {
+        static readonly string AsciiString = string.Concat(Enumerable.Range(32, 127 - 32).Select(i => (char)(short)i));
         static readonly short[] PercentEncodeSet = new short[] { 32, 34, 35, 60, 62, 96, 123, 125, 126 };
 
         [TestMethod]
         public void EscapeUriString_1()
         {
-            var ascii = string.Join("", Enumerable.Range(32, 127 - 32).Select(i => (char)(short)i));
-            var ascii2 = Uri.EscapeUriString(ascii);
+            Console.WriteLine(AsciiString);
+            Console.WriteLine(Uri.EscapeUriString(AsciiString));
+            Console.WriteLine(NetWebUtility.UrlEncode(AsciiString));
+            Console.WriteLine(Uri.EscapeDataString(AsciiString));
+        }
 
+        [TestMethod]
+        public void EscapeUriString_2()
+        {
             Assert.AreEqual("%E3%81%82", Uri.EscapeUriString("あ"));
         }
     }
