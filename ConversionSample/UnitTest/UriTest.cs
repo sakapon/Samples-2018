b1566a7430c02def587107a770a8fb5fe343b084
Keiho Sakapon <git@saka-pon.net>
2018-05-12 20:07:25 +0900

    Update test

--- ConversionSample/UnitTest/UriTest.cs
+++ ConversionSample/UnitTest/UriTest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using ConversionLib;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NetWebUtility = System.Net.WebUtility;
 using WebHttpUtility = System.Web.HttpUtility;
@@ -10,17 +11,30 @@ namespace UnitTest
     [TestClass]
     public class UriTest
     {
-        static readonly string AsciiString = string.Concat(Enumerable.Range(32, 127 - 32).Select(i => (char)(short)i));
+        // 33 symbols
+        static readonly string AsciiString = string.Concat(Enumerable.Range(32, 127 - 32).Select(i => (char)(short)i).Where(c => !char.IsLetter(c) && !char.IsNumber(c)));
         static readonly short[] PercentEncodeSet = new short[] { 32, 34, 35, 60, 62, 96, 123, 125, 126 };
 
         [TestMethod]
         public void EscapeUriString_1()
         {
             Console.WriteLine(AsciiString);
-            Console.WriteLine(Uri.EscapeUriString(AsciiString));
-            Console.WriteLine(NetWebUtility.UrlEncode(AsciiString));
-            Console.WriteLine(WebHttpUtility.UrlEncode(AsciiString));
-            Console.WriteLine(Uri.EscapeDataString(AsciiString));
+
+            UriEncodeTest(Uri.EscapeUriString);
+            UriEncodeTest(NetWebUtility.UrlEncode);
+            UriEncodeTest(s => WebHttpUtility.UrlEncode(s).ToUpperInvariant());
+            UriEncodeTest(Uri.EscapeDataString);
+        }
+
+        static void UriEncodeTest(Func<string, string> urlEncode)
+        {
+            var escaped = AsciiString
+                .Select(c => new { c = c.ToString(), e = urlEncode(c.ToString()) })
+                .Where(_ => _.c != _.e)
+                .ToArray();
+
+            Console.WriteLine(escaped.Select(_ => _.c).ConcatStrings());
+            Console.WriteLine(escaped.Select(_ => _.e).ConcatStrings());
         }
 
         [TestMethod]
