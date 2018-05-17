c93b4b3882ed860e50a7a613018b0f92c2227c3e
Keiho Sakapon <git@saka-pon.net>
2018-05-17 15:56:30 +0900

    Add test

---
+++ ConversionSample/UnitTest/Cryptography/AsymmetricEncryptionTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using ConversionLib.Cryptography;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest.Cryptography
+{
+    [TestClass]
+    public class AsymmetricEncryptionTest
+    {
+        [TestMethod]
+        public void Encrypt()
+        {
+            var data = "P@ssw0rd";
+
+            var keys = AsymmetricEncryption.GenerateKeys();
+            var encrypted = AsymmetricEncryption.Encrypt(data, keys.PublicKey);
+            var decrypted = AsymmetricEncryption.Decrypt(encrypted, keys.KeysPair);
+
+            Console.WriteLine(encrypted);
+            Assert.AreEqual(data, decrypted);
+        }
+    }
+}
