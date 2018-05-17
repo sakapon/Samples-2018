f6929f03fa65650beee89a2e7dc61373057228d4
Keiho Sakapon <git@saka-pon.net>
2018-05-17 12:46:46 +0900

    Add test

--- ConversionSample/UnitTest/Cryptography/HashFunctionsTest.cs
+++ ConversionSample/UnitTest/Cryptography/HashFunctionsTest.cs
@@ -21,6 +21,20 @@ namespace UnitTest.Cryptography
             GenerateHash();
         }
 
+        [TestMethod]
+        public void GenerateHashWithSalt_HMACSHA256Hash()
+        {
+            HashFunctions.Algorithm = new HMACSHA256Hash();
+            GenerateHashWithSalt();
+        }
+
+        [TestMethod]
+        public void GenerateHashWithSalt_Rfc2898Hash()
+        {
+            HashFunctions.Algorithm = new Rfc2898Hash();
+            GenerateHashWithSalt();
+        }
+
         static void GenerateHash()
         {
             var data = "P@ssw0rd";
@@ -35,6 +49,18 @@ namespace UnitTest.Cryptography
             Assert.IsTrue(HashFunctions.VerifyByHash(data, salt, hash));
             Assert.IsFalse(HashFunctions.VerifyByHash(data2, salt, hash));
             Assert.IsFalse(HashFunctions.VerifyByHash(data, salt2, hash));
+        }
+
+        static void GenerateHashWithSalt()
+        {
+            var data = "P@ssw0rd";
+            var data2 = "P@ssw1rd";
+
+            var hash = HashFunctions.GenerateHashWithSalt(data);
+            Console.WriteLine(hash);
+
+            Assert.IsTrue(HashFunctions.VerifyByHashWithSalt(data, hash));
+            Assert.IsFalse(HashFunctions.VerifyByHashWithSalt(data2, hash));
         }
     }
 }
