5c3f33d9a4e15fae7d8e273d1ff9498a824ace32
Keiho Sakapon <git@saka-pon.net>
2018-05-19 16:44:42 +0900

    Add test

---
+++ ConversionSample/UnitTest/Cryptography/SymmetricEncryptionTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Security.Cryptography;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest.Cryptography
+{
+    [TestClass]
+    public class SymmetricEncryptionTest
+    {
+        [TestMethod]
+        public void Create()
+        {
+            var algorithm = SymmetricAlgorithm.Create();
+            Assert.IsInstanceOfType(algorithm, typeof(RijndaelManaged));
+            Assert.AreEqual(256, algorithm.KeySize); // in bits
+            Assert.AreEqual(128, algorithm.BlockSize); // in bits
+        }
+    }
+}
