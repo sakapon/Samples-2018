39b6f615dd097a798de4caa304ca9ad1d33b28b2
Keiho Sakapon <git@saka-pon.net>
2018-05-19 13:17:37 +0900

    Add methods

--- ConversionSample/ConversionLib/Cryptography/HashFunction.cs
+++ ConversionSample/ConversionLib/Cryptography/HashFunction.cs
@@ -10,13 +10,25 @@ namespace ConversionLib.Cryptography
         public abstract int HashLength { get; }
         public abstract int SaltLength { get; }
 
+        public abstract byte[] GenerateHash(byte[] data);
         public abstract byte[] GenerateHash(byte[] data, byte[] salt);
     }
 
     public class HMACSHA256Hash : HashFunctionBase
     {
         public override int HashLength { get; } = 256 / 8;
-        public override int SaltLength { get; } = 128 / 8;
+        public override int SaltLength { get; } = 512 / 8;
+
+        public override byte[] GenerateHash(byte[] data)
+        {
+            if (data == null) throw new ArgumentNullException(nameof(data));
+
+            using (var algorithm = new SHA256Managed())
+            {
+                // The result has 256 bits.
+                return algorithm.ComputeHash(data);
+            }
+        }
 
         public override byte[] GenerateHash(byte[] data, byte[] salt)
         {
@@ -37,6 +49,16 @@ namespace ConversionLib.Cryptography
 
         public override int HashLength { get; } = 256 / 8;
         public override int SaltLength { get; } = 128 / 8;
+
+        public override byte[] GenerateHash(byte[] data)
+        {
+            if (data == null) throw new ArgumentNullException(nameof(data));
+
+            using (var algorithm = new Rfc2898DeriveBytes(data, new byte[SaltLength], Iterations))
+            {
+                return algorithm.GetBytes(HashLength);
+            }
+        }
 
         public override byte[] GenerateHash(byte[] data, byte[] salt)
         {
