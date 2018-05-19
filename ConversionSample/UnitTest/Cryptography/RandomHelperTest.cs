d877394aa2d6ffc256ba8b045c7ebc79bbb134c8
Keiho Sakapon <git@saka-pon.net>
2018-05-19 14:16:39 +0900

    Add test

---
+++ ConversionSample/UnitTest/Cryptography/RandomHelperTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Security.Cryptography;
+using ConversionLib.Cryptography;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest.Cryptography
+{
+    [TestClass]
+    public class RandomHelperTest
+    {
+        [TestMethod]
+        public void Create()
+        {
+            var algorithm = RandomNumberGenerator.Create();
+            Assert.IsInstanceOfType(algorithm, typeof(RNGCryptoServiceProvider));
+        }
+
+        [TestMethod]
+        public void GenerateBase64()
+        {
+            var length = 64;
+            var actual = RandomHelper.GenerateBase64(length);
+            Console.WriteLine(actual);
+            Assert.AreEqual(length, Convert.FromBase64String(actual).Length);
+        }
+    }
+}
