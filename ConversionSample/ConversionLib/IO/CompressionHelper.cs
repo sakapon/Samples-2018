098a9d8f2a533017c9120f770b4df303c23073cb
Keiho Sakapon <git@saka-pon.net>
2018-05-20 19:08:34 +0900

    Add class for compression

---
+++ ConversionSample/ConversionLib/IO/CompressionHelper.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+
+namespace ConversionLib.IO
+{
+    public static class CompressionHelper
+    {
+        // ランダムなデータに適用すると長くなります。
+        // 2 回適用すると長くなります。
+        public static void CompressTo(this Stream input, Stream output)
+        {
+            if (input == null) throw new ArgumentNullException(nameof(input));
+            if (output == null) throw new ArgumentNullException(nameof(output));
+
+            using (var gzip = new GZipStream(output, CompressionMode.Compress, true))
+            {
+                input.CopyTo(gzip);
+            }
+        }
+
+        public static void DecompressTo(this Stream input, Stream output)
+        {
+            if (input == null) throw new ArgumentNullException(nameof(input));
+            if (output == null) throw new ArgumentNullException(nameof(output));
+
+            using (var gzip = new GZipStream(input, CompressionMode.Decompress, true))
+            {
+                gzip.CopyTo(output);
+            }
+        }
+
+        public static byte[] Compress(this byte[] data)
+        {
+            using (var input = new MemoryStream(data))
+            using (var output = new MemoryStream())
+            {
+                input.CompressTo(output);
+                return output.ToArray();
+            }
+        }
+
+        public static byte[] Decompress(this byte[] data)
+        {
+            using (var input = new MemoryStream(data))
+            using (var output = new MemoryStream())
+            {
+                input.DecompressTo(output);
+                return output.ToArray();
+            }
+        }
+
+        public static void Compress(string inputPath, string outputPath)
+        {
+            using (var input = File.OpenRead(inputPath))
+            using (var output = File.Create(outputPath))
+            {
+                input.CompressTo(output);
+            }
+        }
+
+        public static void Decompress(string inputPath, string outputPath)
+        {
+            using (var input = File.OpenRead(inputPath))
+            using (var output = File.Create(outputPath))
+            {
+                input.DecompressTo(output);
+            }
+        }
+    }
+}
---
+++ ConversionSample/UnitTest/IO/CompressionHelperTest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using ConversionLib.Cryptography;
+using ConversionLib.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest.IO
+{
+    [TestClass]
+    public class CompressionHelperTest
+    {
+        [TestMethod]
+        public void Compress_Bytes()
+        {
+            Compress_Bytes_Text(100);
+        }
+
+        static void Compress_Bytes_Text(int dataSize)
+        {
+            var dataText = Convert.ToBase64String(CryptoHelper.GenerateBytes(dataSize));
+            var data = dataText.ToBytes();
+
+            var compressed = data.Compress();
+            var decompressed = compressed.Decompress();
+
+            if (dataSize <= 1024)
+            {
+                Console.WriteLine(dataText);
+                Console.WriteLine(Convert.ToBase64String(compressed));
+            }
+            CollectionAssert.AreEqual(data, decompressed);
+        }
+
+        [TestMethod]
+        public void Compress_Stream()
+        {
+            Compress_Stream_Random(100);
+        }
+
+        static void Compress_Stream_Random(int dataSize)
+        {
+            var data = CryptoHelper.GenerateBytes(dataSize);
+            var compressedStream = new MemoryStream();
+            var decompressedStream = new MemoryStream();
+
+            new MemoryStream(data).CompressTo(compressedStream);
+            var compressed = compressedStream.ToArray();
+            new MemoryStream(compressed).DecompressTo(decompressedStream);
+            var decompressed = decompressedStream.ToArray();
+
+            if (dataSize <= 1024)
+            {
+                Console.WriteLine(Convert.ToBase64String(data));
+                Console.WriteLine(Convert.ToBase64String(compressed));
+            }
+            CollectionAssert.AreEqual(data, decompressed);
+        }
+    }
+}
