3951ae8b12c5865839f27d49db839b3cee40bab8
Keiho Sakapon <git@saka-pon.net>
2018-05-20 10:54:29 +0900

    Add methods

--- ConversionSample/ConversionLib/Cryptography/HybridEncryption.cs
+++ ConversionSample/ConversionLib/Cryptography/HybridEncryption.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 
 namespace ConversionLib.Cryptography
@@ -14,8 +15,8 @@ namespace ConversionLib.Cryptography
             if (publicKey == null) throw new ArgumentNullException(nameof(publicKey));
 
             var commonKey = SymmetricEncryption.GenerateKey();
-            var encryptedData = SymmetricEncryption.Encrypt(data, commonKey);
             var encryptedKey = AsymmetricEncryption.Encrypt(commonKey, publicKey);
+            var encryptedData = SymmetricEncryption.Encrypt(data, commonKey);
 
             CryptoHelper.Concat(encryptedKey, encryptedData, out var result);
             return result;
@@ -44,6 +45,32 @@ namespace ConversionLib.Cryptography
             if (data == null) throw new ArgumentNullException(nameof(data));
 
             return Decrypt(Convert.FromBase64String(data), keysPair).ToText();
+        }
+
+        public static void Encrypt(Stream input, Stream output, string publicKey)
+        {
+            if (input == null) throw new ArgumentNullException(nameof(input));
+            if (output == null) throw new ArgumentNullException(nameof(output));
+            if (publicKey == null) throw new ArgumentNullException(nameof(publicKey));
+
+            var commonKey = SymmetricEncryption.GenerateKey();
+            var encryptedKey = AsymmetricEncryption.Encrypt(commonKey, publicKey);
+
+            output.Write(encryptedKey, 0, encryptedKey.Length);
+            SymmetricEncryption.Encrypt(input, output, commonKey);
+        }
+
+        public static void Decrypt(Stream input, Stream output, string keysPair)
+        {
+            if (input == null) throw new ArgumentNullException(nameof(input));
+            if (output == null) throw new ArgumentNullException(nameof(output));
+            if (keysPair == null) throw new ArgumentNullException(nameof(keysPair));
+
+            var encryptedKey = new byte[AsymmetricEncryption.KeySize];
+            input.Read(encryptedKey, 0, encryptedKey.Length);
+
+            var commonKey = AsymmetricEncryption.Decrypt(encryptedKey, keysPair);
+            SymmetricEncryption.Decrypt(input, output, commonKey);
         }
     }
 }
--- ConversionSample/UnitTest/Cryptography/HybridEncryptionTest.cs
+++ ConversionSample/UnitTest/Cryptography/HybridEncryptionTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using ConversionLib.Cryptography;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -18,6 +19,36 @@ namespace UnitTest.Cryptography
 
             Console.WriteLine(encrypted);
             Assert.AreEqual(data, decrypted);
+        }
+
+        [TestMethod]
+        public void Encrypt_Stream_Short()
+        {
+            Encrypt_Stream(100);
+        }
+
+        [TestMethod]
+        public void Encrypt_Stream_Long()
+        {
+            Encrypt_Stream(10000);
+            Encrypt_Stream(999999);
+        }
+
+        static void Encrypt_Stream(int dataSize)
+        {
+            var data = CryptoHelper.GenerateBytes(dataSize);
+            var keys = HybridEncryption.GenerateKeys();
+            var encryptedStream = new MemoryStream();
+            var decryptedStream = new MemoryStream();
+
+            HybridEncryption.Encrypt(new MemoryStream(data), encryptedStream, keys.PublicKey);
+            var encrypted = encryptedStream.ToArray();
+            HybridEncryption.Decrypt(new MemoryStream(encrypted), decryptedStream, keys.KeysPair);
+            var decrypted = decryptedStream.ToArray();
+
+            if (dataSize <= 1024)
+                Console.WriteLine(Convert.ToBase64String(encrypted));
+            CollectionAssert.AreEqual(data, decrypted);
         }
     }
 }
