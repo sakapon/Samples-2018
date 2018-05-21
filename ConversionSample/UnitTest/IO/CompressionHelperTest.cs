36f681181a8d198320a5f96d6f9b25968ca3b1d0
Keiho Sakapon <git@saka-pon.net>
2018-05-21 18:38:11 +0900

    Update test

--- ConversionSample/UnitTest/IO/CompressionHelperTest.cs
+++ ConversionSample/UnitTest/IO/CompressionHelperTest.cs
@@ -13,6 +13,9 @@ namespace UnitTest.IO
         public void Compress_Bytes()
         {
             Compress_Bytes_Text(100);
+            Compress_Bytes_Text(500);
+            Compress_Bytes_Text(10000);
+            Compress_Bytes_Text(999999);
         }
 
         static void Compress_Bytes_Text(int dataSize)
@@ -25,9 +28,12 @@ namespace UnitTest.IO
 
             if (dataSize <= 1024)
             {
-                Console.WriteLine(dataText);
+                Console.WriteLine(Convert.ToBase64String(data));
                 Console.WriteLine(Convert.ToBase64String(compressed));
             }
+            Console.WriteLine($"Data: {data.Length} Bytes");
+            Console.WriteLine($"Compressed: {compressed.Length} Bytes");
+
             CollectionAssert.AreEqual(data, decompressed);
         }
 
@@ -35,6 +41,9 @@ namespace UnitTest.IO
         public void Compress_Stream()
         {
             Compress_Stream_Random(100);
+            Compress_Stream_Random(500);
+            Compress_Stream_Random(10000);
+            Compress_Stream_Random(999999);
         }
 
         static void Compress_Stream_Random(int dataSize)
@@ -53,6 +62,9 @@ namespace UnitTest.IO
                 Console.WriteLine(Convert.ToBase64String(data));
                 Console.WriteLine(Convert.ToBase64String(compressed));
             }
+            Console.WriteLine($"Data: {data.Length} Bytes");
+            Console.WriteLine($"Compressed: {compressed.Length} Bytes");
+
             CollectionAssert.AreEqual(data, decompressed);
         }
     }
