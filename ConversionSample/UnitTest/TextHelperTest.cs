6dd2b91abf77491adcd275387eb4d9d58dbad749
Keiho Sakapon <git@saka-pon.net>
2018-05-21 22:07:20 +0900

    Add test

---
+++ ConversionSample/UnitTest/TextHelperTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class TextHelperTest
+    {
+        [TestMethod]
+        public void Convert_Base()
+        {
+            var data = 123456;
+            var @base = 16;
+            var text = Convert.ToString(data, @base);
+            var data2 = Convert.ToInt32(text, @base);
+            Assert.AreEqual(data2, data);
+        }
+    }
+}
