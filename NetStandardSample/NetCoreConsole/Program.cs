10a2503cb82a7243ed89676c7e272bb545a9a66e
Keiho Sakapon <git@saka-pon.net>
2018-06-14 04:44:36 +0900

    Create project

---
+++ NetStandardSample/NetCoreConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NetCoreConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
