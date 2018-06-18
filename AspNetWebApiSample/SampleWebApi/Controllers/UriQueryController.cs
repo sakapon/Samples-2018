8933db597bc8ab0c28e7afaa33466ca8d3d8c747
Keiho Sakapon <git@saka-pon.net>
2018-06-18 11:16:59 +0900

    Add test for URL encoding on Web API

---
+++ AspNetWebApiSample/SampleWebApi/Controllers/UriQueryController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using SampleWebApi.Models;
+
+namespace SampleWebApi.Controllers
+{
+    public class UriQueryController : ApiController
+    {
+        // GET api/uriquery/hello
+        // GET api/uriquery?id=hello
+        public string Get(string id)
+        {
+            Debug.WriteLine(id);
+            return id;
+        }
+
+        // POST api/uriquery
+        public string Post(Person person)
+        {
+            Debug.WriteLine(person.Name);
+            return person.Name;
+        }
+    }
+}
---
+++ AspNetWebApiSample/UnitTest/Client/UriQueryTest.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest.Client
+{
+    [TestClass]
+    public class UriQueryTest
+    {
+        const string Alphanumerics = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        const string Rfc3986_UnreservedChars = "-._~"; // 4 symbols
+        const string Rfc3986_ReservedChars = "!#$&'()*+,/:;=?@[]"; // 18 symbols
+        const string Rfc3986_OtherChars = " \"%<>\\^`{|}"; // 11 symbols
+
+        [TestMethod]
+        public void Get_Uri()
+        {
+            void Test(string id)
+            {
+                var uri = "api/uriquery/{0}".FormatUri(id);
+                Console.WriteLine(uri);
+
+                var result = HttpHelper.GetAsync<string>(uri).GetAwaiter().GetResult();
+                Assert.AreEqual(id, result);
+            }
+
+            // Percent Encoding しても使用できない文字があります。
+            Test(Alphanumerics);
+            Test(Rfc3986_UnreservedChars);
+            Test(Rfc3986_ReservedChars);
+            Test(Rfc3986_OtherChars);
+            Test("あ");
+        }
+
+        [TestMethod]
+        public void Get_Query()
+        {
+            void Test(string id)
+            {
+                var uri = "api/uriquery".AddQuery(new { id });
+                Console.WriteLine(uri);
+
+                var result = HttpHelper.GetAsync<string>(uri).GetAwaiter().GetResult();
+                Assert.AreEqual(id, result);
+            }
+
+            Test(Alphanumerics);
+            Test(Rfc3986_UnreservedChars);
+            Test(Rfc3986_ReservedChars);
+            Test(Rfc3986_OtherChars);
+            Test("あ");
+        }
+
+        [TestMethod]
+        public void Post()
+        {
+            void Test(string name)
+            {
+                var result = HttpHelper.PostAsFormAsync<string>("api/uriquery", new { name }).GetAwaiter().GetResult();
+                Assert.AreEqual(name, result);
+            }
+
+            Test(Alphanumerics);
+            Test(Rfc3986_UnreservedChars);
+            Test(Rfc3986_ReservedChars);
+            Test(Rfc3986_OtherChars);
+            Test("あ");
+        }
+    }
+}
