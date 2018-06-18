6d784f8974e5bb36c54c4aa7d192ed3047519d68
Keiho Sakapon <git@saka-pon.net>
2018-06-18 17:40:28 +0900

    Add comments

--- AspNetWebApiSample/SampleWebApi/Controllers/UriQueryController.cs
+++ AspNetWebApiSample/SampleWebApi/Controllers/UriQueryController.cs
@@ -9,22 +9,47 @@ using SampleWebApi.Models;
 
 namespace SampleWebApi.Controllers
 {
+    /// <summary>
+    /// For the test of URL-encoded query and form.
+    /// </summary>
     public class UriQueryController : ApiController
     {
-        // GET api/uriquery/hello
-        // GET api/uriquery?id=hello
+        /// <summary>
+        /// Echoes the specified ID.
+        /// </summary>
+        /// <param name="id">The ID.</param>
+        /// <returns>The ID.</returns>
+        /// <remarks>
+        /// GET api/uriquery/hello
+        /// GET api/uriquery?id=hello
+        /// </remarks>
         public string Get(string id)
         {
             Debug.WriteLine(id);
             return id;
         }
 
-        // GET api/uriquery/wildcard/hello
+        /// <summary>
+        /// Echoes the specified ID.
+        /// </summary>
+        /// <param name="id">The ID.</param>
+        /// <returns>The ID.</returns>
+        /// <remarks>
+        /// GET api/uriquery/wildcard/hello
+        /// GET api/uriquery/wildcard/?id=hello
+        /// </remarks>
         [HttpGet]
         [Route("api/UriQuery/Wildcard/{*id}")]
         public string Wildcard(string id) => Get(id);
 
-        // POST api/uriquery
+        /// <summary>
+        /// Echoes the specified name.
+        /// </summary>
+        /// <param name="person">The person.</param>
+        /// <returns>The name.</returns>
+        /// <remarks>
+        /// POST api/uriquery
+        /// </remarks>
         public string Post(Person person)
         {
             Debug.WriteLine(person.Name);
