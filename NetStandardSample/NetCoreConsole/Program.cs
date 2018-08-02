ca34d5de71e38ab7ea229a72c06b71d814f1ed3c
Keiho Sakapon <git@saka-pon.net>
2018-08-02 10:37:35 +0900

    Add package reference to library

--- NetStandardSample/NetCoreConsole/Program.cs
+++ NetStandardSample/NetCoreConsole/Program.cs
@@ -8,6 +8,7 @@ namespace NetCoreConsole
         static void Main(string[] args)
         {
             Console.WriteLine(RandomHelper.GenerateBase64(16));
+            Console.WriteLine(RandomHelper2.GenerateBase64s());
             Console.WriteLine("Press [Enter] key to exit.");
             Console.ReadLine();
         }
--- NetStandardSample/NetFW47Console/Program.cs
+++ NetStandardSample/NetFW47Console/Program.cs
@@ -12,6 +12,7 @@ namespace NetFW47Console
         static void Main(string[] args)
         {
             Console.WriteLine(RandomHelper.GenerateBase64(16));
+            Console.WriteLine(RandomHelper2.GenerateBase64s());
             Console.WriteLine("Press [Enter] key to exit.");
             Console.ReadLine();
         }
