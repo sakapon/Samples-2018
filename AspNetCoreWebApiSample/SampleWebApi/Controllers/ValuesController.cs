ddb15aa23c2843007479c2a79a43f8b4abc4ed79
Keiho Sakapon <git@saka-pon.net>
2018-08-05 11:57:20 +0900

    Add comments

--- AspNetCoreWebApiSample/SampleWebApi/Controllers/ValuesController.cs
+++ AspNetCoreWebApiSample/SampleWebApi/Controllers/ValuesController.cs
@@ -6,37 +6,62 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace SampleWebApi.Controllers
 {
+    /// <summary>
+    /// Represents string data.
+    /// </summary>
     [Produces("application/json")]
     [Route("api/[controller]")]
     public class ValuesController : Controller
     {
-        // GET api/values
+        /// <summary>
+        /// Gets all values.
+        /// </summary>
+        /// <returns>All values.</returns>
+        /// <remarks>GET api/values</remarks>
         [HttpGet]
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
-        // GET api/values/5
+        /// <summary>
+        /// Gets the value for the specified ID.
+        /// </summary>
+        /// <param name="id">The ID.</param>
+        /// <returns>The value.</returns>
+        /// <remarks>GET api/values/5</remarks>
         [HttpGet("{id}")]
         public string Get(int id)
         {
             return "value";
         }
 
-        // POST api/values
+        /// <summary>
+        /// Posts the new value.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <remarks>POST api/values</remarks>
         [HttpPost]
         public void Post([FromBody]string value)
         {
         }
 
-        // PUT api/values/5
+        /// <summary>
+        ///  Updates the value for the specified ID.
+        /// </summary>
+        /// <param name="id">The ID.</param>
+        /// <param name="value">The value.</param>
+        /// <remarks>PUT api/values/5</remarks>
         [HttpPut("{id}")]
         public void Put(int id, [FromBody]string value)
         {
         }
 
-        // DELETE api/values/5
+        /// <summary>
+        ///  Deletes the value for the specified ID.
+        /// </summary>
+        /// <param name="id">The ID.</param>
+        /// <remarks>DELETE api/values/5</remarks>
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
