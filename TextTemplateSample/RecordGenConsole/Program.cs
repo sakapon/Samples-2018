f7d6dda50c736faa2590f7b5944743a42b89163c
Keiho Sakapon <git@saka-pon.net>
2018-09-06 17:19:47 +0900

    Add test

--- TextTemplateSample/RecordGenConsole/Program.cs
+++ TextTemplateSample/RecordGenConsole/Program.cs
@@ -6,7 +6,15 @@ namespace RecordGenConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var person = new Person(4, "Shiro", DateTime.UtcNow, null);
+
+            var n = new Number(3, 2.5, DayOfWeek.Sunday);
+            Console.WriteLine(n.Scaled);
         }
+    }
+
+    public partial class Number
+    {
+        public double Scaled => (Scale ?? 1) * N;
     }
 }
