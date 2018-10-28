615425814f3593fc2fc0539f2817ef518b901648
Keiho Sakapon <git@saka-pon.net>
2018-10-28 19:10:50 +0900

    Update test

--- ProgrammingSample/UnitTest/UnionBytesTest.cs
+++ ProgrammingSample/UnitTest/UnionBytesTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -16,23 +17,33 @@ namespace UnitTest
             var bytes1_u = Bytes32.Union(bytes1);
             var bytes2_u = Bytes32.Union(bytes2);
 
+            var sum = bytes1.Zip(bytes2, (x, y) => (byte)(x + y)).ToArray();
+
             var bytes3_u = bytes1_u.Zip(bytes2_u, (x, y) => x + y).ToArray();
-            var bytes3 = Bytes32.Spread(bytes3_u);
+            var sum_union = Bytes32.Spread(bytes3_u);
+
+            CollectionAssert.AreEqual(sum, sum_union);
         }
 
         [TestMethod]
         public void Bytes_32_Time()
         {
-            var bytes1 = Enumerable.Range(0, 32).Select(i => (byte)i).ToArray();
-            var bytes2 = Enumerable.Range(0, 32).Select(i => (byte)(3 * i)).ToArray();
+            var bytes1 = Enumerable.Range(0, 100_000_000).Select(i => (byte)i).ToArray();
+            var bytes2 = Enumerable.Range(0, 100_000_000).Select(i => (byte)(3 * i)).ToArray();
 
-            var result_normal = new byte[bytes1.Length];
+            var sw1 = Stopwatch.StartNew();
+            var sum = new byte[bytes1.Length];
             for (var i = 0; i < bytes1.Length; i++)
-                result_normal[i] = (byte)(bytes1[i] + bytes2[i]);
+                sum[i] = (byte)(bytes1[i] + bytes2[i]);
+            sw1.Stop();
+            Console.WriteLine(sw1.Elapsed);
 
-            var result_union = new byte[bytes1.Length];
+            var sw2 = Stopwatch.StartNew();
+            var sum_union = new byte[bytes1.Length];
             for (var i = 0; i < bytes1.Length; i += 4)
-                (new Bytes32(bytes1, i) + new Bytes32(bytes2, i)).CopyTo(result_union, i);
+                (new Bytes32(bytes1, i) + new Bytes32(bytes2, i)).CopyTo(sum_union, i);
+            sw2.Stop();
+            Console.WriteLine(sw2.Elapsed);
         }
     }
 }
