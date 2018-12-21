7a2f4b005342bc0239d307de61d44663807694a4
Keiho Sakapon <git@saka-pon.net>
2018-12-21 17:26:07 +0900

    Create project

---
+++ SyntaxTreeSample/NumericConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NumericConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
