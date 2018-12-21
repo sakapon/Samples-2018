8a8924ba5c28e98245b744d7ff7de0d6dafdd492
Keiho Sakapon <git@saka-pon.net>
2018-12-21 21:59:05 +0900

    Show variables on console

--- SyntaxTreeSample/DebuggerConsole/Program.cs
+++ SyntaxTreeSample/DebuggerConsole/Program.cs
@@ -1,15 +1,39 @@
 ﻿using System;
+using System.CodeDom.Compiler;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
+using DebuggerLib;
 
 namespace DebuggerConsole
 {
     class Program
     {
+        const string SourcePath = @"..\..\..\NumericConsole\Program.cs";
+        const string GeneratedPath = @"Program.g.cs";
+
         static void Main(string[] args)
         {
+            var sourceCode = File.ReadAllText(SourcePath);
+            var generatedCode = SyntaxHelper.InsertBreakpoints(sourceCode);
+            File.WriteAllText(GeneratedPath, generatedCode, Encoding.UTF8);
+
+            var provider = CodeDomProvider.CreateProvider("CSharp");
+            var compilerOption = new CompilerParameters(new[] { "DebuggerLib.dll" }) { GenerateExecutable = true };
+            var compilerResult = provider.CompileAssemblyFromFile(compilerOption, GeneratedPath);
+            if (compilerResult.Errors.HasErrors) return;
+
+            DebugHelper.InfoNotified += (spanStart, spanLength, variables) =>
+            {
+                Console.WriteLine(string.Join(", ", variables.Select(p => $"{p.Key}: {p.Value}")));
+                Console.WriteLine(sourceCode.Substring(spanStart, spanLength));
+                Thread.Sleep(1000);
+            };
+
+            var entryPoint = compilerResult.CompiledAssembly.EntryPoint;
+            entryPoint.Invoke(null, new object[] { new string[0] });
         }
     }
 }
