39569028c13de79daca9b561e1d249b2d545df13
Keiho Sakapon <git@saka-pon.net>
2018-12-22 13:38:09 +0900

    Add AppModel class

---
+++ SyntaxTreeSample/TickTackDebugger/AppModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using DebuggerLib;
+using Reactive.Bindings;
+
+namespace TickTackDebugger
+{
+    public class AppModel
+    {
+        public TargetProgram TargetProgram { get; }
+
+        public ReactiveProperty<double> ExecutionInterval { get; } = new ReactiveProperty<double>(0.5);
+        public ReactiveProperty<IDictionary<string, object>> Variables { get; } = new ReactiveProperty<IDictionary<string, object>>(new Dictionary<string, object>());
+
+        public AppModel()
+        {
+            TargetProgram = new TargetProgram();
+
+            // Registers the action for breakpoints.
+            DebugHelper.InfoNotified += (spanStart, spanLength, variables) =>
+            {
+                Variables.Value = variables;
+                Thread.Sleep(TimeSpan.FromSeconds(ExecutionInterval.Value));
+            };
+        }
+
+        public void StartDebugging()
+        {
+            Task.Run(() => TargetProgram.StartDebugging());
+        }
+    }
+}
