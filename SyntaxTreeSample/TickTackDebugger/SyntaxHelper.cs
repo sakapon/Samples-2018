38db0dbfa9b42cfafc86aa6537e52fbac8da9aa2
Keiho Sakapon <git@saka-pon.net>
2018-12-22 12:13:19 +0900

    Add classes

---
+++ SyntaxTreeSample/TickTackDebugger/SyntaxHelper.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Text;
+using DebugStatement = System.ValueTuple<Microsoft.CodeAnalysis.CSharp.Syntax.StatementSyntax, string[]>;
+
+namespace TickTackDebugger
+{
+    public static class SyntaxHelper
+    {
+        public static string InsertBreakpoints(string sourceCode)
+        {
+            var root = ParseText(sourceCode);
+
+            var method = root.DescendantNodes()
+                .OfType<MethodDeclarationSyntax>()
+                .First(m => m.Identifier.ValueText == "Main");
+            var statements = DetectStatements(method);
+
+            var result = sourceCode;
+            foreach (var (statement, variables) in statements.Reverse())
+            {
+                var (span, debugIndex) = GetSpan(statement);
+                result = result.Insert(debugIndex, $"DebuggerLib.DebugHelper.NotifyInfo({span.Start}, {span.Length}, {ToDictionaryText(variables)});\r\n");
+            }
+            return result;
+        }
+
+        public static CompilationUnitSyntax ParseText(string text)
+        {
+            var tree = CSharpSyntaxTree.ParseText(text);
+            var diagnostics = tree.GetDiagnostics().ToArray();
+            if (diagnostics.Length > 0) return null;
+
+            return tree.GetCompilationUnitRoot();
+        }
+
+        public static DebugStatement[] DetectStatements(MethodDeclarationSyntax method)
+        {
+            var statements = new List<DebugStatement>();
+            DetectStatements(method.Body, statements, new List<(string, SyntaxNode)>());
+            return statements.ToArray();
+        }
+
+        static void DetectStatements(SyntaxNode node, List<DebugStatement> statements, List<(string name, SyntaxNode scope)> variables)
+        {
+            // Adds variables.
+            if (node is VariableDeclarationSyntax varSyntax)
+            {
+                var varNames = varSyntax.Variables.Select(v => v.Identifier.ValueText).ToArray();
+                var scope = ((node.Parent is LocalDeclarationStatementSyntax) ? node.Parent : node)
+                    .Ancestors()
+                    .First(n => n is StatementSyntax);
+
+                variables.AddRange(varNames.Select(v => (v, scope)));
+            }
+
+            // Maps variables to the statement.
+            if ((node is StatementSyntax statement) &&
+                !(node is BlockSyntax) &&
+                !(node is BreakStatementSyntax))
+                statements.Add((statement, variables.Select(v => v.name).ToArray()));
+
+            // Recursively.
+            foreach (var child in node.ChildNodes())
+                DetectStatements(child, statements, variables);
+
+            // Maps variables to the last line of the block.
+            if (node is BlockSyntax block)
+                statements.Add((block, variables.Select(v => v.name).ToArray()));
+
+            // Clears variables out of the scope.
+            if (node is StatementSyntax)
+                for (var i = variables.Count - 1; i >= 0; i--)
+                    if (variables[i].scope == node)
+                        variables.RemoveAt(i);
+                    else
+                        break;
+        }
+
+        static (TextSpan, int) GetSpan(StatementSyntax statement)
+        {
+            switch (statement)
+            {
+                case ForStatementSyntax f:
+                    var span = new TextSpan(f.ForKeyword.Span.Start, f.CloseParenToken.Span.End - f.ForKeyword.Span.Start);
+                    return (span, statement.FullSpan.Start);
+                case BlockSyntax b:
+                    return (b.CloseBraceToken.Span, b.CloseBraceToken.FullSpan.Start);
+                default:
+                    return (statement.Span, statement.FullSpan.Start);
+            }
+        }
+
+        static string ToDictionaryText(string[] variables) =>
+            $"new Dictionary<string, object> {{ {string.Join(", ", variables.Select(v => $"{{ \"{v}\", {v} }}"))} }}";
+    }
+}
---
+++ SyntaxTreeSample/TickTackDebugger/TargetProgram.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace TickTackDebugger
+{
+    public class TargetProgram
+    {
+        const string SourcePath = @"..\..\..\NumericConsole\Program.cs";
+        const string GeneratedPath = @"Program.g.cs";
+
+        MethodInfo entryPoint;
+
+        public void Initialize()
+        {
+            // Generates the code for debugging.
+            var sourceCode = File.ReadAllText(SourcePath);
+            var generatedCode = SyntaxHelper.InsertBreakpoints(sourceCode);
+            File.WriteAllText(GeneratedPath, generatedCode, Encoding.UTF8);
+
+            // Compiles and loads the assembly.
+            var provider = CodeDomProvider.CreateProvider("CSharp");
+            var compilerOption = new CompilerParameters(new[] { "DebuggerLib.dll" }) { GenerateExecutable = true };
+            var compilerResult = provider.CompileAssemblyFromFile(compilerOption, GeneratedPath);
+            if (compilerResult.Errors.HasErrors) return;
+
+            entryPoint = compilerResult.CompiledAssembly.EntryPoint;
+        }
+
+        public void StartDebugging()
+        {
+            entryPoint.Invoke(null, new object[] { new string[0] });
+        }
+    }
+}
