9d8e781165caa12f0c5c4323142341a5c9a07779
Keiho Sakapon <git@saka-pon.net>
2018-12-23 08:42:27 +0900

    Update default program

--- SyntaxTreeSample/TickTackDebugger/Resources/Program.cs
+++ SyntaxTreeSample/TickTackDebugger/Resources/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var text = "Hello, World!";
+        Console.WriteLine(text);
     }
 }
