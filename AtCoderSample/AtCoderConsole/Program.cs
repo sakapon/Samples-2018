6fe8a71208c608d2acec526a6a950ed72eac5995
Keiho Sakapon <git@saka-pon.net>
2019-08-04 16:20:16 +0900

    Create project

---
+++ AtCoderSample/AtCoderConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AtCoderConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
